c531b7e5103286c0a074bc8c70fe3f5b4d30221f
DW31 TALDEA <ik012982i9@H012982.NET>
2023-11-06 12:34:16 +0100

    Añadido ejercicios

---
+++ src/Ejercicios/Ejercicio1.java
@@ -0,0 +1,15 @@
+class Ejercicio1{
+
+    public static void main(String[] args) {
+
+        System.out.println("Imprimiendo del uno al cuatro...");
+
+        for(int i = 1; i <= 4; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("Saliendo...");
+
+    }
+
+}
