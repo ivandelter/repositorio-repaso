a296d1e13edaab8a4de6fd965b670a4f6fc42073
Iván Delgado Terroso <idelgadote23dw@ikzubirimanteo.com>
2023-11-06 13:18:48 +0100

    Mejorado el Ejercicio 1

--- src/Ejercicios/Ejercicio1.java
+++ src/Ejercicios/Ejercicio1.java
@@ -1,3 +1,7 @@
+package Ejercicios;
+
+import java.util.Arrays;
+
 class Ejercicio1{
 
     public static void main(String[] args) {
@@ -12,15 +16,23 @@ class Ejercicio1{
         System.out.println("Se va a generar 5 números entre 0 y 20 y se comparará para saber si es menor o mayor a " + num);
 
         for(int i = 1; i <= 5; i++){
-            int random = (int)Math.random() * 20;
+            int random = (int)(Math.random() * 20);
             if(random > num){
-                System.out.println(random " es mayor a " + num);
-            } else if(random < num){
-                System.out.println(random " es menor a " + num);
+                System.out.println(random + " es mayor a " + num);
+            }else if(random < num){
+                System.out.println(random +  " es menor a " + num);
             }else{
-                System.out.println(random + " y " + num + " son igaules.");
+                System.out.println(random + " y " + num + " son iguales.");
             }
         }
+
+        System.out.println("Haciendo una prueba con arrays");
+
+        int[] array = {0, 1, 2, 3, 4};
+        String[] arrayTexto = {"Iván", "Oihan", "Adam"};
+
+        System.out.println(Arrays.toString(array));
+        System.out.println(String.join(", ", arrayTexto));
 
         System.out.println("Saliendo...");
 
